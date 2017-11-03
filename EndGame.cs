929fd8e894c2363497d0f418d92faa97dfb53b98
mziv <30710901+mziv@users.noreply.github.com>
2017-11-02 22:46:47 -0700

    Add files via upload

---
+++ EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    public GameObject text;
+
+	public void OnTriggerEnter()
+    {
+        text.SetActive(true);
+    }
+}
