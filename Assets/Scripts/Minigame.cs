6a28e643b7fa7cbbbb9b4002bb611d9a95712b17
mziv <30710901+mziv@users.noreply.github.com>
2017-11-05 16:00:20 -0800

    Added Maya's test prefab

---
+++ Assets/Scripts/Minigame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minigame : MonoBehaviour {
+
+    public float speed;
+    private Image[] images;
+    private Image player;
+    RectTransform rectTransform;
+
+    // Use this for initialization
+    void Start () {
+        initializePlayer();
+    }
+
+    void initializePlayer()
+    {
+        images = GetComponentsInChildren<Image>();
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (images[i].CompareTag("virtualPlayer"))
+            {
+                player = images[i];
+            }
+        }
+        rectTransform = player.GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("w"))
+        {
+            print("key check worked");
+
+            Vector3 currentPos = rectTransform.anchoredPosition;
+            player.GetComponent<RectTransform>().anchoredPosition = new Vector3(currentPos.x + speed * Time.deltaTime, currentPos.y, currentPos.z);
+
+        }
+
+    }
+}
