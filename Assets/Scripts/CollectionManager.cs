84c295e4a2b1d13853e0c183e497bcbf8739ba82
Richard Lin <yifengl@stanford.edu>
2017-11-11 20:29:55 -0800

    inventory
    inventory can now be opened and closed

---
+++ Assets/Scripts/CollectionManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionManager : MonoBehaviour {
+
+    public Canvas collection;
+    private Text[] texts;
+    private const int dataCardOffset = 0;
+    private const int acquiredOffset = 3;
+    private const int missingOffset = 6;
+
+    //state determines which texts to display when the canvas is displayed
+    private const bool inventoryState = true;
+    private bool[] dataCardState = new bool[3];
+    private bool[] acquiredState = new bool[3];
+    private bool[] missingState = new bool[3];
+
+    private bool inventoryOpen = false;
+
+	// Use this for initialization
+	void Start () {
+        //collect all texts on the inventory canvas
+        texts = collection.GetComponentsInChildren<Text>();
+        initializeInventoryState();
+        disableInventory();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("i") && !inventoryOpen)
+        {
+            displayInventory();
+            inventoryOpen = true;
+        }
+        else if(Input.GetKeyDown("i") && inventoryOpen)
+        {
+            disableInventory();
+            inventoryOpen = false;
+        }
+	}
+
+    void displayInventory()
+    {
+        inventory().enabled = inventoryState;
+        for(int index = 1; index <= 3; index++)
+        {
+            dataCard(index).enabled = dataCardState[index - 1]; //-1 because state is 0 indexed
+            acquired(index).enabled = acquiredState[index - 1];
+            missing(index).enabled = missingState[index - 1];
+        }
+        collection.enabled = true;
+    }
+
+    void disableInventory()
+    {
+        collection.enabled = false;
+    }
+
+    void initializeInventoryState()
+    {
+        for (int index = 0; index < 3; index++)
+        {
+            dataCardState[index] = true;
+            acquiredState[index] = false;
+            missingState[index] = true;
+        }
+    }
+
+    Text inventory()
+    {
+        return texts[0];
+    }
+
+    Text dataCard(int index)
+    {
+        return texts[dataCardOffset + index];
+    }
+
+    Text acquired(int index)
+    {
+        return texts[acquiredOffset + index];
+    }
+
+    Text missing(int index)
+    {
+        return texts[missingOffset + index];
+    }
+}
