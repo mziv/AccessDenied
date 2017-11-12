20f85d1394692dcae331644e94f9befe63284464
Richard Lin <yifengl@stanford.edu>
2017-11-11 18:24:04 -0800

    collectable made
    collectable can now be collected(destroyed)

---
+++ Assets/Scripts/CollectableTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CollectableTrigger : MonoBehaviour {
+
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        collect();
+    }
+
+    void collect()
+    {
+        if (Input.GetKeyDown("e") && playerInRange)
+        {
+            Debug.Log("collected");
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        playerInRange = true;
+    }
+
+    void OnTriggerExit()
+    {
+        playerInRange = false;
+    }
+}
