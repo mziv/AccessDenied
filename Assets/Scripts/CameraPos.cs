13ce02b5bd60ba6c57e84a35e433b8db27cd0664
richardlyf <richardlyf@users.noreply.github.com>
2017-11-21 22:38:30 -0800

    created half a level
    Created half a level and added a bunch of lighting!
    Fixed the camera and player control a little bit.
    Still need to fix the camera going into walls. Tried a solution that worked but didn't really like the effect. Need to think of something nicer

--- Assets/Scripts/CameraPos.cs
+++ Assets/Scripts/CameraPos.cs
@@ -6,16 +6,24 @@ public class CameraPos : MonoBehaviour {
     private GameObject player;
     public float upDownOffset = 3.0f;
     public bool checkPlayer = false;
+    private bool cameraTouchingWall = false;
+    private float playerToCamDistance;
 
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerToCamDistance = getPlayerCamDistance();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         LocatePlayer();
-        this.transform.position = player.transform.position + cameraPos;
+        //Debug.Log(playerToCamDistance);
+        //Debug.Log(getPlayerCamDistance());
+        Vector3 newCameraPos = translatePosToV3();
+        //isPlayerFarFromWall();
+        //if(!cameraTouchingWall) this.transform.position = player.transform.position + newCameraPos;
+        this.transform.position = player.transform.position + newCameraPos;
         this.transform.LookAt(player.transform.position + upDownOffset * transform.up);
     }
 
@@ -26,6 +34,37 @@ public class CameraPos : MonoBehaviour {
             player = GameObject.FindGameObjectWithTag("Player");
             FindObjectOfType<Worker>().locateTarget = true;
             checkPlayer = false;
+        }
+    }
+
+    Vector3 translatePosToV3()
+    {
+        float faceDirectionMagnitude = Mathf.Sqrt(Mathf.Pow(player.transform.forward.x, 2) + Mathf.Pow(player.transform.forward.z, 2));
+        float scaleX = cameraPos.x / faceDirectionMagnitude;
+        float scaleZ = cameraPos.z / faceDirectionMagnitude;
+
+        float x = player.transform.forward.x * scaleX * -1 + player.transform.forward.z * scaleZ;
+        float z = player.transform.forward.z * scaleX * -1 + player.transform.forward.x * scaleZ * -1;
+
+        return new Vector3(x, cameraPos.y , z);
+    }
+
+    void isPlayerFarFromWall()
+    {
+        if (playerToCamDistance <= getPlayerCamDistance()) cameraTouchingWall = false;
+    }
+
+    float getPlayerCamDistance()
+    {
+        return Mathf.Sqrt(Mathf.Pow((player.transform.position - this.transform.position).x, 2) + Mathf.Pow((player.transform.position - this.transform.position).z, 2));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Wall")
+        {
+            cameraTouchingWall = true;
+            //Debug.Log("touch");
         }
     }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
     public float speed = 6.0F;
-    public float rotationSpeed = 10.0f;
+    public float rotationSpeed = 60.0f;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
@@ -13,16 +13,36 @@ public class PlayerControl : MonoBehaviour {
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(0, 0 ,0);
 
         if (controller.isGrounded)
         {
+            //FPS version
+            if (Input.GetKey("w"))
+            {
+                moveDirection = gameObject.transform.forward;
+            }else if (Input.GetKey("s"))
+            {
+                moveDirection = -gameObject.transform.forward;
+            }
+
+            if (Input.GetKey("a"))
+            {
+                transform.Rotate(-Vector3.up * rotationSpeed * Time.deltaTime);
+            }else if (Input.GetKey("d"))
+            {
+                transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+            }
+
+            //TPS version
+            /*
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             if (moveDirection != Vector3.zero)
             {
                 Quaternion qTo = Quaternion.LookRotation(moveDirection);
                 transform.rotation = Quaternion.Slerp(transform.rotation, qTo, Time.deltaTime * rotationSpeed);
 
-            }
+            }*/
                 moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
