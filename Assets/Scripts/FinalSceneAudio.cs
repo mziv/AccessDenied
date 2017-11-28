5a574aab21d24199d4e9774d143f8d19ac64e766
mziv <30710901+mziv@users.noreply.github.com>
2017-11-28 00:00:49 -0800

    Fixed AI Audio

---
+++ Assets/Scripts/FinalSceneAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalSceneAudio : MonoBehaviour {
+
+    private AudioSource AIAudio;
+    public AudioSource mainAudio;
+
+	// Use this for initialization
+	void Start () {
+        AIAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter()
+    {
+        PlayTrack("Sound/AI/Final Scene/AI monologue");
+        mainAudio.Stop();
+    }
+
+    public void PlayTrack(string trackLocation)
+    {
+        Stop();
+        AIAudio.PlayOneShot(Resources.Load<AudioClip>(trackLocation), 1);
+    }
+
+    public void Stop()
+    {
+        AIAudio.Stop();
+    }
+}
