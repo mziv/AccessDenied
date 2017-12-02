149541de053e31e046c7bdc29223e9692f5ec64f
richardlyf <richardlyf@users.noreply.github.com>
2017-12-01 20:17:28 -0800

    fixed lighting in settings
    need to include shaders in project settings to have volumetric light show up built game

---
+++ Assets/Scripts/EGGTerminal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EGGTerminal : MonoBehaviour {
+
+    private GameObject player;
+    private bool inRadius = false;
+    private CollectionManager inventory;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        inventory = FindObjectOfType<CollectionManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e") && inRadius && player.tag == "Player")
+        {
+            if (hasPermission())
+            {
+                Debug.Log("opened EGG");
+            }
+            else
+            {
+                Debug.Log("Need all three cards");
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRadius = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRadius = false;
+        }
+    }
+
+    private bool hasPermission()
+    {
+        return (inventory.card1 && inventory.card2 && inventory.card3);
+    }
+}
