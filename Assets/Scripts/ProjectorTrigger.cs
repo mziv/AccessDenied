c051c4a86e3e036c9d5a245dd00f5f8ae899f35f
richardlyf <richardlyf@users.noreply.github.com>
2017-12-01 18:06:21 -0800

    minor fixes
    map fixes. added projector to prevent BS in first hallway

---
+++ Assets/Scripts/ProjectorTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorTrigger : MonoBehaviour {
+
+    private bool inRange = false;
+    private bool projectorOn = false;
+    public GameObject particle;
+
+    private void Start()
+    {
+        particle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKeyUp("e") && inRange && !projectorOn)
+        {
+            particle.SetActive(true);
+            projectorOn = true;
+        }
+        else if(Input.GetKeyUp("e") && inRange && projectorOn)
+        {
+            particle.SetActive(false);
+            projectorOn = false;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+}
